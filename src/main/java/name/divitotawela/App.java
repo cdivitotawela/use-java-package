1c3c8539d6ae5f0aedf7ba3f3202659bf6f433c4
Chaminda Divitotawela <cdivitotawela@gmail.com>
2024-03-19 10:12:03 +1000

    test

---
+++ src/main/java/name/divitotawela/App.java
@@ -0,0 +1,13 @@
+package name.divitotawela;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
